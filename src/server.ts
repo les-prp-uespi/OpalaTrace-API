2c9fa72b8cc6be9890f2e746b41be8044e418da8
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2024-05-25 21:48:05 -0300

    Refatorações no servidor da API

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import router from "./router";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+app.use(router);
+
+app.get("/", (req, res) => {console.log("rodando")});
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return res.status(400).json({ message: err.message });
+    }
+  
+    return res
+      .status(500)
+      .json({ status: "erro", message: "Erro interno do Servidor" });
+  });
+ 
+app.listen(3000, () => console.log("Servidor rodando "));
