3b4ff6c92e06c28318e42ee0d895f194a5650b31
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2024-05-27 22:50:23 -0300

    Criação do middeware de autenticação

---
+++ src/middleware/auth.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+type TokenPayload = {
+    id: String;
+    iat: number;
+    exp: number;
+}
+
+export function AutenticacaoMiddeware(req: Request, res: Response, next: NextFunction) {
+
+    const { autorizacao } = req.headers;
+
+    if (!autorizacao) {
+        return res.status(401).json({ error: "Token não fornecido." });
+    }
+
+    const [, token] = autorizacao.toString().split(" ");
+
+    try {
+        const decoded = verify(token, "secret");
+        const { id } = decoded as TokenPayload;
+        req.usuarioId = id;
+        next();
+
+    } catch (error) {
+        return res.status(401).json({ error: "Token inválido." })
+    }
+
+}
