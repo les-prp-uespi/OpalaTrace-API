649361bfac65d9595efd5a4fbbe36157db5b8149
LeoneRSantos <leonnerodriguez1@gmail.com>
2024-08-16 11:04:17 -0300

    Identação em CriarUsuarioController

--- src/controller/creat_user_controller.ts
+++ src/controller/creat_user_controller.ts
@@ -1,12 +1,14 @@
 import { Request, Response } from "express";
-import { CreatUser } from "./creat_user"
+import { Usuario } from "../models/usuario"
 import { prisma } from "../dataBase/PrismaClient";
+import { z } from 'zod'
 
 export class CriarUsuarioController {
     async handle(req: Request, res: Response) {
         const { nome, email, senha, id, id_funcao } = req.body;
-        const criarusuario = new CreatUser();
+        const criarusuario = new Usuario();
         const result = await criarusuario.execute({
+            data,
             id,
             nome,
             email,
@@ -20,7 +22,7 @@ export class CriarUsuarioController {
 
             return res.status(200).json(result);
         }
-        return res.json("Erro ao cadastrar");
+        return res.json("Erro ao cadastrar o agente.");
     }
 
     async index(req: Request, res: Response) {
